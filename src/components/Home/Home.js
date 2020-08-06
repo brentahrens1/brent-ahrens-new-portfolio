fd0eac1c7117e68a8a57eb2aead1031642d57100
Brent Ahrens <cidida@gmail.com>
2020-08-06 07:45:22 -0700

    rough styling for projects

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,24 @@
 import React from 'react'
 import '../../sass/_home.scss'
 
+import { projects } from '../../const/projects'
+
 const Home = () => {
     return (
         <div className="project">
-            <div className="project__inner">
-                <div className="project__content">
-                    <h1 className="project__title">Site Name</h1>
-                    <div className="project__image">
-                        <img src="" alt="" />
+            {
+                projects.map((project, idx) => 
+                    <div className="project__inner" style={{background: project.background}}>
+                        <div className="project__content">
+                            <h1 className="project__title">{project.title}</h1>
+                            <div className="project__image">
+                                <img src={project.img} alt={project.title} />
+                            </div>
+                            <p className="project__description">{project.description}</p>
+                        </div>
                     </div>
-                    <p className="project__description">This is a site I worked on and used all these mega dope techs for bruh!</p>
-                </div>
-            </div>
-            <div className="project__inner">
-                <div className="project__content">
-                    <h1 className="project__title">Site Name</h1>
-                    <div className="project__image">
-                        <img src="" alt="" />
-                    </div>
-                    <p className="project__description">This is a site I worked on and used all these mega dope techs for bruh!</p>
-                </div>
-            </div>
+                )
+            }
         </div>
     )
 }
---
+++ src/const/projects.js
@@ -0,0 +1,31 @@
+import youngPlanet from "../images/websites/young-planet.png"
+import aceReader from "../images/websites/reader.png"
+import breslin from "../images/websites/breslin.png"
+import aceHotel from "../images/websites/acehotel.png"
+
+export const projects = [
+    {
+        title: "Young Planet",
+        img: youngPlanet,
+        description: "This is the info bro this is a super dope site.",
+        background: "#D8D8D8"
+    },
+    {
+        title: "Ace Reader",
+        img: aceReader,
+        description: "This is the info bro this is a super dope site.",
+        background: "#A5BFE0"
+    },
+    {
+        title: "Breslin",
+        img: breslin,
+        description: "This is the info bro this is a super dope site.",
+        background: "#1F7750"
+    },
+    {
+        title: "Ace Hotel",
+        img: aceHotel,
+        description: "This is the info bro this is a super dope site.",
+        background: "#fff"
+    },
+]
