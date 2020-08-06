19420649c1b19caa9d3322cde60bb06c80bb827f
Brent Ahrens <cidida@gmail.com>
2020-08-06 08:10:56 -0700

    adds links and link titles to js

--- src/const/projects.js
+++ src/const/projects.js
@@ -8,24 +8,32 @@ export const projects = [
         title: "Young Planet",
         img: youngPlanet,
         description: "This is the info bro this is a super dope site.",
+        link: "https://www.cidida.com",
+        linkTitle: "spinyoungplanet.com",
         background: "#D8D8D8"
     },
     {
         title: "Ace Reader",
         img: aceReader,
         description: "This is the info bro this is a super dope site.",
+        link: "http://www.reader.acehotel.com/",
+        linkTitle: "reader.acehotel.com",
         background: "#A5BFE0"
     },
     {
         title: "Breslin",
         img: breslin,
         description: "This is the info bro this is a super dope site.",
+        link: "https://www.breslinnyc.com/",
+        linkTitle: "breslinnyc.com",
         background: "#1F7750"
     },
     {
         title: "Ace Hotel",
         img: aceHotel,
         description: "This is the info bro this is a super dope site.",
+        link: "https://www.acehotel.com/",
+        linkTitle: "acehotel.com",
         background: "#fff"
     },
 ]
