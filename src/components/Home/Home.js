6fcffea37b59ba5f7c024784432304b351bb183e
Brent Ahrens <cidida@gmail.com>
2020-08-06 02:31:54 -0700

    fixes overflow issue with contact button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,6 +13,15 @@ const Home = () => {
                     <p className="project__description">This is a site I worked on and used all these mega dope techs for bruh!</p>
                 </div>
             </div>
+            <div className="project__inner">
+                <div className="project__content">
+                    <h1 className="project__title">Site Name</h1>
+                    <div className="project__image">
+                        <img src="" alt="" />
+                    </div>
+                    <p className="project__description">This is a site I worked on and used all these mega dope techs for bruh!</p>
+                </div>
+            </div>
         </div>
     )
 }
