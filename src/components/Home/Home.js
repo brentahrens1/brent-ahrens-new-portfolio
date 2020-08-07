89f3bda305804ce52dc48c8a53a916e9941d4d26
brentahrens1 <cidida@gmail.com>
2020-08-06 18:52:07 -0700

    adds hover effect to projects

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../../sass/_home.scss'
 
 import { projects } from '../../const/projects'
 
 const Home = () => {
-    let linkOpen = false
-
-    const imageEnter = () => {
-        linkOpen = true
-    }
-
-    const imageLeave = () => {
-        linkOpen = false
-    }
 
     return (
         <div className="project">
@@ -21,11 +12,11 @@ const Home = () => {
                     <div key={idx} className="project__inner" style={{background: project.background}}>
                         <div className="project__content">
                             <h1 className="project__title">{project.title}</h1>
-                            <div className="project__image" onMouseEnter={imageEnter} onMouseLeave={imageLeave}>
+                            <div className="project__image">
                                 <img src={project.img} alt={project.title} />
+                                <h1 className="project__link-popup"><a href={project.link} target="_blank" rel="noopener noreferrer">{project.linkTitle}</a></h1>
                             </div>
                             <p className="project__description">{project.description}</p>
-                            <h1 className={ linkOpen ? "project__link-popup" : "project__link-hide"}><a href={project.link} target="_blank" rel="noopener noreferrer">{project.linkTitle}</a></h1>
                         </div>
                     </div>
                 )
