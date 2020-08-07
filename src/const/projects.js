843c2be6744136a40e7c2a0e497e5a397365ae23
Brent Ahrens <cidida@gmail.com>
2020-08-07 06:35:02 -0700

    style tweaks to nav / home / projects for responsivness

--- src/const/projects.js
+++ src/const/projects.js
@@ -7,7 +7,7 @@ export const projects = [
     {
         title: "Young Planet",
         img: youngPlanet,
-        description: "This is the info bro this is a super dope site.",
+        description: "Young Planet is a Brooklyn based Hip Hop duo. Built with React js and SASS",
         link: "https://www.cidida.com",
         linkTitle: "spinyoungplanet.com",
         background: "#D8D8D8"
@@ -15,7 +15,7 @@ export const projects = [
     {
         title: "Ace Reader",
         img: aceReader,
-        description: "This is the info bro this is a super dope site.",
+        description: "A blog for all Ace Hotel properties. A Wordpress site I contributed to with CSS and javascript echancements.",
         link: "http://reader.acehotel.com/",
         linkTitle: "reader.acehotel.com",
         background: "#A5BFE0"
@@ -23,7 +23,7 @@ export const projects = [
     {
         title: "Breslin",
         img: breslin,
-        description: "This is the info bro this is a super dope site.",
+        description: "A famed Michelin gastro pub in New York City. In collaboration with our design team, I built the custom Squarespace site in developer mode using css and javascript.",
         link: "https://www.breslinnyc.com/",
         linkTitle: "breslinnyc.com",
         background: "#1F7750"
@@ -31,7 +31,7 @@ export const projects = [
     {
         title: "Ace Hotel",
         img: aceHotel,
-        description: "This is the info bro this is a super dope site.",
+        description: "A chain of boutique hotels with locations across the country and abroad. I build and manage property sites in the Wagtail admin and regularly implement CSS enhancements, content updates, bug fixes and SEO improvements.",
         link: "https://www.acehotel.com/",
         linkTitle: "acehotel.com",
         background: "#fff"
