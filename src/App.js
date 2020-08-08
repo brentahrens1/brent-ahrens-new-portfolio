dc66fbffe6979b59d6a3338694ac7f42791277ee
brentahrens1 <cidida@gmail.com>
2020-08-07 17:01:18 -0700

    adds about page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
 import Index from './components/Index/Index'
+import About from './components/About/About'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/index" exact component={Index} />
+        <Route path="/about" exact component={About} />
       </Switch>
     </div>
   );
