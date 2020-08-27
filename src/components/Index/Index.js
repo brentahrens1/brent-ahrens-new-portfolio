0a509bfda5d302fed9d46181ace41c64924325d7
Brent Ahrens <cidida@gmail.com>
2020-08-27 08:16:48 -0700

    adds styling tweaks to navbar and home

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -9,13 +9,15 @@ const Index = () => {
             {
                 projects.map((project, idx) => 
                     <div key={idx} className="index__inner" style={{background: project.background}}>
-                        <div className="index__content">
-                            <h1 className="index__title">{project.title}</h1>
-                            <div className="index__image">
-                                <img src={project.img} alt={project.title} />
-                                <h1 className="index__link-popup"><a href={project.link} target="_blank" rel="noopener noreferrer">{project.linkTitle}</a></h1>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            <div className="index__content">
+                                <h1 className="index__title">{project.title}</h1>
+                                <div className="index__image">
+                                    <img src={project.img} alt={project.title} />
+                                    <h1 className="index__link-popup"><a href={project.link} target="_blank" rel="noopener noreferrer">{project.linkTitle}</a></h1>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                         <h1 className="index__link-mobile"><a href={project.link} target="_blank" rel="noopener noreferrer">{project.linkTitle}</a></h1>
                     </div>
                 )
